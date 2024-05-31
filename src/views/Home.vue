5e33cfcfaeb817335cc05f57006ad22332830bf3
SergioSuarezF <101756534+SergioSuarezF@users.noreply.github.com>
2024-05-30 23:22:25 -0500

    prueba con app.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<v-app :style="{background: $vuetify.theme.themes.dark.background}">
+    <NavBar />
+</v-app>
+    
+</template>
+
+<script>
+
+    import NavBar from '@/components/NavBar.vue';
+
+    export default {
+        name: 'Home',
+
+        components: {
+            NavBar
+        },
+    }
+
+</script>
